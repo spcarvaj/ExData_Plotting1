b8638bc59f2bcfe43dc00250b49eb13a5599a9fd
SCARVAJAL <spcarvaj@gmail.com>
2015-07-11 14:22:37 -0400

    Plot 4 Correct
    Product correct plot 4

--- plot4.R
+++ plot4.R
@@ -25,13 +25,13 @@ data02$datetime <- as.POSIXct(datetime)
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data02, {
   plot(datetime, Global_active_power, type="l", ylab="Global Active Power", xlab="")
-  plot(datetime, Voltage, type="l", ylab="Voltage", xlab="datatime")
+  plot(datetime, Voltage, type="l", ylab="Voltage", xlab="datetime")
   plot(datetime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab="", col="black")
   lines(datetime, Sub_metering_2, col="red")
   lines(datetime, Sub_metering_3, col="blue")
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2.5, bty="n", cex=0.5,
           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(datetime, Global_reactive_power, type="l", ylab="Global Reactive Power (kilowatts)",xlab="datetime")
+  plot(datetime, Global_reactive_power, type="l", ylab="Global Reactive Power",xlab="datetime")
 })
 
 ## Create PNG file
